2cf315b7992e20a2bd363e6a89db2a3f00f94779
GaoMax <max.gao@icloud.com>
2021-11-30 15:27:46 +0800

    Update netflix-change-plan.py

--- netflix-change-plan.py
+++ netflix-change-plan.py
@@ -13,6 +13,23 @@ BASIS = 0
 STANDARD = 1
 PREMIUM = 2
 
+def set_chrome_options() -> None:
+    """Sets chrome options for Selenium.
+    Chrome options for headless browser is enabled.
+    """
+    chrome_options = Options()
+    chrome_options.add_argument("--headless")
+    chrome_options.add_argument("--no-sandbox")
+    chrome_options.add_argument("--disable-dev-shm-usage")
+    chrome_prefs = {}
+    chrome_options.experimental_options["prefs"] = chrome_prefs
+    chrome_prefs["profile.default_content_settings"] = {"images": 2}
+    return chrome_options
+
+if __name__ == "__main__":
+    driver = webdriver.Chrome(options=chrome_options)
+    # Do stuff with your driver
+    driver.close()
 
 def change_plan(plan):
     driver.get("https://www.netflix.com/ChangePlan")
@@ -72,7 +89,7 @@ def start_webdriver():
     global driver
     options = Options()
     options.add_argument('--headless')
-    driver = webdriver.Chrome(options=options)
+    driver = webdriver.Chrome(options=chrome_options)
     driver.implicitly_wait(3)
 
 
